7a4b3db17315d137d78dd108a93a6ddb8e3a8a20
Alberto Win <gobbi.alberto@gene.com>
2016-11-17 18:47:34 -0800

    added -rndFract

--- src/autocorrelator/apps/SdfSplicer.java
+++ src/autocorrelator/apps/SdfSplicer.java
@@ -16,6 +16,8 @@ along with the AutoCorrelator. If not, see <http://www.gnu.org/licenses/>.
 */
 package autocorrelator.apps;
 
+import java.util.Random;
+
 import openeye.oechem.*;
 
 
@@ -29,84 +31,103 @@ public class SdfSplicer
 {  private static final String EXPLAIN=
          "sdfSplicer -start n [-end|count] n -in fn -out fn [-skipped fn]\n\n" +
          "   This will cut out a region from an SDF file\n" +
-         "\t-in.........input file (any OE filetype),  for sdtin use .type.\n" +
-         "\t-out........output file (any OE filetype), for stdout use .type.\n" +
+         "\t-in ........input file (any OE filetype),  for sdtin use .type.\n" +
+         "\t-out .......output file (any OE filetype), for stdout use .type.\n" +
          "\t-skipped ...output file for records not in -out\n" +
          "\t-start n ...First molecule to include in output (default=1)\n" +
-         "\t-end n......First molecule to be excluded from output (default all)\n"+
-         "\t-count n....How many molecules to output (default none)\n"+
-         "\t-readAll....continue reading input even if no more output is produced\n"+
+         "\t-end n .....First molecule to be excluded from output (default all)\n"+
+         "\t-count n ...How many molecules to output (default none)\n"+
+         "\t-rndFract ..Output randFract random records\n"+
+         "\t-rndSeed ...Random Seed fro rndFract\n"+
+         "\t-readAll ...continue reading input even if no more output is produced\n"+
          "\t            use this in pipes to enable preceeding commands to see all input\n"+
          "\t\t          start must be < then end.\n\n";
 
   public static void main(String[] args)
    {  CommandLineParser cParser;
       String[] modes    = { "-readAll" };
-      String[] parms    = { "-start", "-end", "-in", "-out", "-skipped", "-count" };
+      String[] parms    = { "-start", "-end", "-in", "-out", "-skipped", "-count",
+                            "-rndFract", "-rndSeed" };
       String[] reqParms = {"-in", "-out" };
-      
+
       cParser = new CommandLineParser(EXPLAIN,0,0,args,modes,parms,reqParms);
-      
+
       int start=1;
       int end  = Integer.MAX_VALUE;
 
       if(cParser.wasGiven("-start"))
          start = Integer.parseInt(cParser.getValue("-start"));
-      
+
       if(cParser.wasGiven("-end"))
          end = Integer.parseInt(cParser.getValue("-end"));
-      
+
       if(cParser.wasGiven("-count"))
          end = start + Integer.parseInt(cParser.getValue("-count"));
-      
+
+      Random rnd = null;
+      double rndFract = 2D;
+      if( cParser.wasGiven("-rndFract") )
+      {  rndFract = Double.parseDouble(cParser.getValue("-rndFract"));
+         if(cParser.wasGiven("-rndSeed"))
+            rnd = new Random(Long.parseLong(cParser.getValue("-rndSeed")));
+         else
+            rnd = new Random();
+         if(cParser.wasGiven("-count"))
+         {  System.err.println("\n-count not supported with -rndFract!\n");
+            return;
+         }
+      }
+
       if(cParser.wasGiven("-end") && cParser.wasGiven("-count"))
       {  System.err.println("only one of -end and -count is allowed!\n");
          System.err.println(EXPLAIN);
          System.exit(1);
       }
-      
+
       if(start >= end)
       {  System.err.println(EXPLAIN);
          System.exit(1);
       }
-      
-      
+
+
       String in = cParser.getValue("-in");
       String out = cParser.getValue("-out");
       String skipped = cParser.getValue("-skipped");
       boolean readAll = cParser.wasGiven("-readAll") || skipped != null;
-      
+
       oemolistream ifs = new oemolistream(in);
       oemolostream ofs = new oemolostream(out);
       oemolostream ofsSkipped = null;
       if( skipped != null ) ofsSkipped = new oemolostream(skipped);
-      
+
       OEGraphMol mol = new OEGraphMol();
       int count = 0;
-      while (oechem.OEReadMolecule(ifs, mol)) 
+      while (oechem.OEReadMolecule(ifs, mol))
       {  count++;
          if( count >= end) break;
-         
-         if( count < start) 
-         {  if( ofsSkipped != null ) 
+
+         if( count < start)
+         {  if( ofsSkipped != null )
                oechem.OEWriteMolecule(ofsSkipped, mol);
             continue;
          }
-         
-         oechem.OEWriteMolecule(ofs, mol);
-         mol.Clear();
+
+         if( rnd == null || rnd.nextDouble() < rndFract)
+            oechem.OEWriteMolecule(ofs, mol);
+         else
+            oechem.OEWriteMolecule(ofsSkipped, mol);
       }
-      
+
       if( readAll )
       {  while (oechem.OEReadMolecule(ifs, mol))
-         {  if( ofsSkipped != null ) 
+         {  if( ofsSkipped != null )
                oechem.OEWriteMolecule(ofsSkipped, mol);
          }
       }
-            
+
       ifs.close();
       ofs.close();
-      if( ofsSkipped != null ) 
+      if( ofsSkipped != null )
       {  ofsSkipped.close();
          ofsSkipped.delete();
       }
