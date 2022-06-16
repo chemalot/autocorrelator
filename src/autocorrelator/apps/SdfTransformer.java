3ed7330cd64be59c8897fb435630d7e4bb1db8cb
Alberto <gobbi.alberto@gene.com>
2022-06-15 17:32:20 -0700

    fixed scaffold parsing bug

--- src/autocorrelator/apps/SdfTransformer.java
+++ src/autocorrelator/apps/SdfTransformer.java
@@ -496,7 +496,7 @@ public class SdfTransformer
 
          cPos += at.length();
 
-         if( cPos >= scaffoldSma.length() ) return "";
+         if( cPos >= scaffoldSma.length() ) break;
 
          String oth = getNextOther(scaffoldSma,cPos);
          left .append(oth);
@@ -752,7 +752,7 @@ class MyUniMolecularTransform implements MyTransform
    MyUniMolecularTransform(String smirks, String name)
    {  this.trans = new OEUniMolecularRxn(smirks, true);
       if( ! trans.IsValid() )
-         throw new IllegalArgumentException(name);
+         throw new IllegalArgumentException(name + " " + smirks);
       this.name = name;
    }
 
